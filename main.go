db0bd45dd45b7b2183848e8aa262928e9911175f
Suthar345Piyush <piyushsuthar524@gmail.com>
2024-12-25 23:57:05 +0530

    first commit

---
+++ main.go
@@ -0,0 +1,15 @@
+package main
+
+import (
+	"fmt"
+	"mylearning/myUtil"
+)
+
+func main() {
+	fmt.Println("Hello i am learning Golang")
+	fmt.Println("Using golang to excel in tech")
+	fmt.Println("I am frontend dev")
+	fmt.Println("I am a second year ug student")
+
+	myUtil.PrintMessage("hello bois")
+}
