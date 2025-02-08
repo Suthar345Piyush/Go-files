48ea56dce8f6072600b4305fd576cbc283166e76
Piyush <Piyushsuthar524@gmail.com>
2025-02-08 17:53:49 +0530

    Add files via upload

---
+++ generics/go.mod
@@ -0,0 +1,5 @@
+module generics
+
+go 1.23.4
+
+require golang.org/x/exp v0.0.0-20250207012021-f9890c6ad9f3
--- generics/main.go
+++ generics/main.go
@@ -1,36 +1,116 @@
-//generics  in golang
+//Generics  in golang
+
+//Generics  also known as  parametric polymorphism
+// it  enables us  to write code that operates on multiple  types without specifying  types
+//generics means  jinka koi specific type nhi hota hai , koi specific type of  input/ouptut parameters nhi hota hai
+
+// generics  in golang  has :
+//1 . Type  parameters -> it  like  placeholder , in the function initialization
+//2. Type  constraints -> restrict the  types, that can be  used as  type  arguments
+
+// simple code :
+
+// package main
+
+// import "fmt"
+
+// // T is a  type  parameter
+// // any is  constraint , it  is  built in , any type can be  used
+// func PrintSlice[T any](s []T) {
+// 	for _, v := range s {
+// 		fmt.Println(v)
+// 	}
+// }
+
+// func main() {
+// 	intSlice := []int{1, 2, 3}
+// 	stringSlice := []string{"hello", "world"}
+
+// 	PrintSlice[string](stringSlice)
+// 	PrintSlice[int](intSlice)
+// }
+
+// PROG : 2 finding  min in the  slice
+
+// package main
+
+// import (
+// 	"fmt"
+
+// 	"golang.org/x/exp/constraints"
+// )
+
+// func Min[T constraints.Ordered](s []T) T {
+
+// 	//panic  stops  the  normal excution of the current  goroutine
+// 	if len(s) == 0 {
+// 		panic("empty slice")
+// 	}
+
+// 	min := s[0]
+// 	for _, v := range s {
+// 		if v < min {
+// 			min = v
+// 		}
+// 	}
+// 	return min
+// }
+
+// func main() {
+// 	intSlice := []int{3, 1, 4, 2}
+// 	floatSlice := []float64{3.14, 1.61, 4.67, 2.71}
+
+// 	fmt.Println(Min[int](intSlice))
+// 	fmt.Println(Min[float64](floatSlice))
+// }
+
+//PROG-3 Impelamenting a  data structure(stack)
 
 package main
 
 import "fmt"
 
-//heres any means  it can be of any time
-//we cn write  interface{} at the  place  of the  any
-
-func printSlice[T int | string | bool](items []T) {
-	for _, item := range items {
-		fmt.Println(item)
-	}
+type Stack[T any] struct {
+	data []T
 }
 
-// func printSliceString(items []string) {
-// 	for _, item := range items {
-// 		fmt.Println(item)
-// 	}
-// }
+func (s *Stack[T]) Push(v T) {
+	s.data = append(s.data, v)
+}
+
+func (s *Stack[T]) Pop() (T, bool) {
+	if len(s.data) == 0 {
+		var zero T
+		return zero, false
+	}
+
+	lastIndex := len(s.data) - 1
+	value := s.data[lastIndex]
+	s.data = s.data[:lastIndex]
+	return value, true
+}
+
+func (s *Stack[T]) Size() int {
+	return len(s.data)
+}
 
 func main() {
 
-	//nums := []int{1 ,2, 3, 4, 5}
-	names := []string{"a", "b", "c", "d"}
-	names2 := []bool{true, false, true, false}
+	intStack := Stack[int]{}
+	intStack.Push(1)
+	intStack.Push(2)
+	intStack.Push(3)
 
-	// names := []int{1, 2, 3, 79090, 8}
-	printSlice(names)
-	printSlice(names2)
+	fmt.Println(intStack.Pop())
+	fmt.Println(intStack.Size())
 
-	// printSliceString(names)
+	stringStack := Stack[string]{}
+	stringStack.Push("piyush")
+	stringStack.Push("suthar")
+
+	fmt.Println(stringStack.Pop())
+	fmt.Println(stringStack.Size())
+
 }
 
-//notes :
-// we have  to write multiple  functions with some  changes  in the  parameters , so avoid  this  problem we  can use  the  generics  in golang
+// Generics in Go provide a powerful way to write reusable and type-safe code.
